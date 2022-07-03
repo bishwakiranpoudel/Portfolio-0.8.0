4d228c9b6398be783a430295ae6fbfea0899b299
Bishwa Kiran Poudel <bishwakiran725@gmail.com>
2022-07-03 18:46:43 +0545

    might regret

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 import AnimatedRoutes from './AnimatedRoutes';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
+import LiquidSwipe from 'react-liquidswipe';
 
 function App() {
-  
-  return (
-    <div className="App">
+  let components=[];
+  components.push(
+    <>
+     <div className="App">
       
-     <div className="border-screen">
-     <div className="screen">
-        <Router>
+      <div className="border-screen">
+      <div className="screen">
+         <Router>
+           
+             <AnimatedRoutes/>
           
-            <AnimatedRoutes/>
-         
-        </Router>
+         </Router>
+       </div>
       </div>
      </div>
-    </div>
+    </>
   );
+  components.push(
+    <>
+     <div className="App2" >
+      
+      <div className="border-screen">
+      <div className="screen">
+         <Router>
+           
+             <AnimatedRoutes/>
+          
+         </Router>
+       </div>
+      </div>
+     </div>
+    </>
+  );
+
+  
+  return <LiquidSwipe components={components}/>;
 }
 
 export default App;
